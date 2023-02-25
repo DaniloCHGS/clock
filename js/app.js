352f48b4c718db7ff162d4ce6b34e71ca50fa130
DaniloCHGS <danilochagasbm@gmail.com>
2023-02-25 14:06:18 -0300

    Ajuste das funções de relógio

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
-import { hourCalculator, getSeconds, getHours } from "./lib/hourCalculator.js"
+import { getPercentage, getSeconds } from "./lib/clock.js";
 
-const clock = document.querySelector('.clock')
-const counterClock = document.querySelector('.counter-clock')
+const counterClock = document.querySelector(".counter-clock");
 
-const currentHour = getHours()
-const hourLimit = 18
+const hourLimit = "17:00:00";
 
-counterClock.style.height = `${hourCalculator(hourLimit)}%`
-clock.setAttribute('data-hour', currentHour)
+var percentage = getPercentage(new Date().toLocaleTimeString(), hourLimit);
+
+counterClock.style.height = percentage;
 
 setInterval(() => {
-    if (currentHour <= hourLimit) {
-        counterClock.style.height = `${hourCalculator(hourLimit)}%`
-    }
-    return
-}, getSeconds(5))
+  percentage = getPercentage(new Date().toLocaleTimeString(), hourLimit);
+  counterClock.style.height = percentage;
+}, getSeconds(1));
---
+++ js/lib/clock.js
@@ -0,0 +1,21 @@
+const getSeconds = (second) => second * 1000;
+
+const timeToMilliseconds = (time) => {
+  const [hours, minutes, seconds] = time.split(":").map(Number);
+  return ((hours * 60 + minutes) * 60 + seconds) * 1000;
+};
+
+const getPercentage = (currentHour, targetHour) => {
+  const hourInMilliseconds = timeToMilliseconds(currentHour);
+  const targetHourInMilliseconds = timeToMilliseconds(targetHour);
+  const percentage = Math.round(
+    (hourInMilliseconds * 100) / targetHourInMilliseconds
+  );
+  console.log(
+    `Hora atual ${currentHour} - Hora destino ${targetHour} - Porcentagem de alcance ${percentage}%`
+  );
+
+  return `${percentage}%`;
+};
+
+export { getPercentage, getSeconds };
