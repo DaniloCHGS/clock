e45187606f493d3c41d0a9d816b59db4d488174d
DaniloCHGS <danilochagasbm@gmail.com>
2023-02-25 14:37:52 -0300

    Display de hora e data

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ import { getPercentage, getSeconds } from "./lib/clock.js";
 
 const counterClock = document.querySelector(".counter-clock");
 
-const hourLimit = "17:00:00";
+const hourLimit = "23:59:59";
+// const hourLimit = "14:36:00";
 
 var percentage = getPercentage(new Date().toLocaleTimeString(), hourLimit);
 
@@ -11,4 +12,19 @@ counterClock.style.height = percentage;
 setInterval(() => {
   percentage = getPercentage(new Date().toLocaleTimeString(), hourLimit);
   counterClock.style.height = percentage;
+
+  if (percentage === "100%") {
+    counterClock.classList.add("done");
+    return;
+  }
+}, getSeconds(1));
+
+const hourElement = document.querySelector(".display h1");
+hourElement.textContent = new Date().toLocaleTimeString();
+
+const dateElement = document.querySelector(".display h2");
+dateElement.textContent = new Date().toLocaleDateString();
+
+setInterval(() => {
+  hourElement.textContent = new Date().toLocaleTimeString();
 }, getSeconds(1));
